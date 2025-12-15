7fe9377da52cb41a37c3a689eef8ebbbffa0a495
Даниил Шеметов <lego.den.2005@mail.ru>
2025-12-15 03:29:36 +0300

    changed size parameter 32 -> 64 to slow down test

--- tasks/shemetov_d_gauss_filter_linear/tests/performance/main.cpp
+++ tasks/shemetov_d_gauss_filter_linear/tests/performance/main.cpp
@@ -45,7 +45,7 @@ TEST(GaussFilterPerfExtraTest, SmallMatrixSEQ) {
 }
 
 TEST(GaussFilterPerfExtraTest, SmallMatrixMPI) {
-  const int size = 32;
+  const int size = 64;
   InType input(size, std::vector<uint8_t>(size, 100));
   input[size / 2][size / 2] = 255;
 
